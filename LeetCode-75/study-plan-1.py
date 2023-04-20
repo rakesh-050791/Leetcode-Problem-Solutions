3ca629ee63f7c3a890a8b64e3cd676e667d87273
rakesh050791 <rakesh050791@gmail.com>
2023-04-21 00:54:24 +0200

    Array | 724. Find Pivot Index

--- LeetCode-75/study-plan-1.py
+++ LeetCode-75/study-plan-1.py
@@ -7,8 +7,6 @@
 
 # Return the running sum of nums.
 
- 
-
 # Example 1:
 
 # Input: nums = [1,2,3,4]
@@ -36,4 +34,75 @@ class Solution:
         return pf
 
 
-# 2 : 
+# Complexity Analysis
+
+# Time complexity: O(n) where n is the length of the input array. This is because we use a single loop that iterates over the entire array to calculate the running sum.
+
+# Space complexity: O(1) since we don't use any additional space to find the running sum. Note that we do not take into consideration the space occupied by the output array.
+
+
+
+# 2 : 724. Find Pivot Index
+# Given an array of integers nums, calculate the pivot index of this array.
+
+# The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+
+# If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+
+# Return the leftmost pivot index. If no such index exists, return -1.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,7,3,6,5,6]
+# Output: 3
+# Explanation:
+# The pivot index is 3.
+# Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
+# Right sum = nums[4] + nums[5] = 5 + 6 = 11
+# Example 2:
+
+# Input: nums = [1,2,3]
+# Output: -1
+# Explanation:
+# There is no index that satisfies the conditions in the problem statement.
+# Example 3:
+
+# Input: nums = [2,1,-1]
+# Output: 0
+# Explanation:
+# The pivot index is 0.
+# Left sum = 0 (no elements to the left of index 0)
+# Right sum = nums[1] + nums[2] = 1 + -1 = 0
+
+
+# Solution Approach : 
+
+# We need to quickly compute the sum of values to the left and the right of every index.
+
+# Let's say we knew totalSum as the sum of the numbers, and we are at index i. If we knew the sum of numbers leftsum that are to the left of index i, then the other sum to the right of the index would just be totalSum - nums[i] - leftsum.
+
+# As such, we only need to know about leftsum to check whether an index is a pivot index in constant time. Let's do that: as we iterate through candidate indexes i, we will maintain the correct value of leftsum.
+
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+
+        totalSum = sum(nums)
+        leftSum = 0
+
+        for indx, val in enumerate(nums):
+            if leftSum == (totalSum - nums[indx] - leftSum):
+                return indx
+            leftSum += val
+        
+        return -1
+
+
+# Complexity Analysis
+
+# Time Complexity: O(N), where N is the length of nums.
+
+# Space Complexity: O(1), the space used by leftsum and totalSum.
+
